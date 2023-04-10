b26d51eb495fa531850b6aad0bb796da3d16a034
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-04-10 20:15:23 +0700

    Add basic calculator components and CSS styling for calculator frame

--- script.js
+++ script.js
@@ -21,3 +21,32 @@ function modulus(num1, num2) {
 function power(num, pow) {
    return Math.pow(num, pow);
 }
+
+function operate(ops, num1, num2) {
+   switch (ops) {
+      case "+":
+         add(num1, num2);
+         break;
+      case "-":
+         subtract(num1, num2);
+         break;
+      case "*":
+         multiply(num1, num2);
+         break;
+      case "/":
+         divide(num1, num2);
+         break;
+      case "%":
+         modulus(num1, num2);
+         break;
+      case "^":
+         power(num1, num2);
+         break;
+   }
+}
+
+function processDivInjectedByLiveServer() {
+   //    const htmlBody = document.querySelector("body");
+   const divByLiveServer = document.querySelector("div");
+   divByLiveServer.remove();
+}
